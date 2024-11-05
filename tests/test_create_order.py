339ff3b989f612ec1c467d49821c52d4ec210da5
AleksandrBorisenkov <gizmo6666@ya.ru>
2024-11-05 17:29:30 +0300

    правка по диплому

--- tests/test_create_order.py
+++ tests/test_create_order.py
@@ -1,37 +1,25 @@
-import json
-from random import random
-
 import allure
 import requests
-import random
 
 import data
-from data import get_ingredients, post_login_user, exist_user_payload, post_create_order
+from data import post_login_user, exist_user_payload, post_create_order
 from helper import password_generator
 
-
-def return_random_ingredient():
-    get_info = requests.get(get_ingredients)
-    data_text = json.loads(get_info.text)
-    ingredients = [element['_id'] for element in data_text['data']]
-    return random.choice(ingredients)
 
 @allure.description('Класс создания заказа для не авторизованных пользователей.')
 class TestUnauthorizedCreateOrder:
 
     @allure.title('Создаем заказ если не авторизован и есть ингредиент. Ожидаем успех 200')
-    def test_unauthorized_create_order_with_ingredients_true(self):
-        ingredients = {"ingredients": [return_random_ingredient()]}
+    def test_unauthorized_create_order_with_ingredients_true(self, return_random_ingredient):
+        ingredients = {"ingredients": [return_random_ingredient]}
         create_order = requests.post(post_create_order, ingredients)
         assert create_order.json()["success"] == True and create_order.status_code == 200
-        print(create_order.json(), create_order.status_code)
 
     @allure.title('Создаем заказ если не авторизован и нет ингридиента. Ожидаем ошибку 400')
     def test_unauthorized_create_order_without_ingredients_false(self):
         ingredients = {"ingredients": []}
         create_order = requests.post(post_create_order, ingredients)
         assert create_order.json() == data.no_ingredient_error_400 and create_order.status_code == 400
-        print(create_order.json(), create_order.status_code)
 
     @allure.title('Создаем заказ если не авторизован и невалидный HASH ингридиента. Ожидаем ошибку 500')
     def test_unauthorized_create_order_with_badhash_ingredients_false(self):
@@ -45,8 +33,8 @@ class TestUnauthorizedCreateOrder:
 class TestLoginUserCreateOrder:
 
     @allure.title('Создаем заказ если юзер авторизован, передан токен, и есть ингредиент. Ожидаем успех 200')
-    def test_login_user_create_order_with_ingredients_true(self):
-        ingredients = {"ingredients": [return_random_ingredient()]}
+    def test_login_user_create_order_with_ingredients_true(self, return_random_ingredient):
+        ingredients = {"ingredients": [return_random_ingredient]}
         response = requests.post(post_login_user, data=exist_user_payload)
         auth_token = response.json()["accessToken"]
         assert "accessToken" in response.json() and response.status_code == 200
